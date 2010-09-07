538baef9caa1864d974637ed7652732f1b2c165a
Manohar Vanga <mvanga@mvanga-laptop.(none)>
2010-09-08 00:56:34 +0530

    Fixed stack initialization. Everything works!

--- boot.s
+++ boot.s
@@ -4,7 +4,7 @@
 .extern kernel_main
 
 interrupt_vectors:
-	b _unused_vector
+	b _start_bach
 	b _unused_vector
 	b _unused_vector
 	b _unused_vector
@@ -14,10 +14,14 @@ interrupt_vectors:
 	b _unused_vector
 
 _unused_vector:
+	ldr sp, =vectors
+	sub sp, sp, #10
 	adr r0, unused_msg
 	bl puts
 	b died
 _start_bach:
+	ldr sp, =vectors
+	sub sp, sp, #10
 	bl kernel_main
 died:
 	adr r0, died_msg
